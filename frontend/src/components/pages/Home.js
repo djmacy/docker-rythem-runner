d81ea0ab6c1be67496def29999d80b6ba724be2d
David Macy <dmacy@carroll.edu>
2024-11-30 13:21:28 -0700

    added temp login for test server. I know react .env is not secure but just trying to keep normal people out

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -7,6 +7,12 @@ const Home = () => {
 
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [isPremiumUser, setIsPremiumUser] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [isAccessGranted, setIsAccessGranted] = useState(false);
+
+    const spotifyUser = process.env.REACT_APP_SPOTIFY_USER;
+    const spotifyAccess = process.env.REACT_APP_SPOTIFY_PASS;
 
     useEffect(() => {
         const fetchAuthAndPremiumStatus = async () => {
@@ -29,13 +35,45 @@ const Home = () => {
         fetchAuthAndPremiumStatus();
     }, []);
 
+    const handleLogin = (e) => {
+        e.preventDefault();
+        if (username === spotifyUser && password === spotifyAccess) {
+            setIsAccessGranted(true);
+        } else {
+            alert("Incorrect username or password!");
+        }
+    };
+
+    if (!isAccessGranted && !isAuthenticated) {
+        return (
+            <div style={{alignItems: 'center', justifyContent: 'center', textAlign: 'center'}}>
+                <h2 style={{marginTop: '100px', color: '#fff'}}>App is still under development.</h2>
+                <h2 style={{color: '#fff'}}>Please log in to the test server.</h2>
+                <form onSubmit={handleLogin}>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
+        );
+    }
+
     return (
         <div>
             <Header/>
-            {isAuthenticated && !isPremiumUser ? <ModalPremium/> : null}
+            {isAuthenticated && !isPremiumUser ? <ModalPremium /> : null}
         </div>
-
     );
-}
+};
 
 export default Home;
